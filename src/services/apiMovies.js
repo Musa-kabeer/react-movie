6a27e850d13ff5322818b5907d3847f05b374501
Bakodev <musaabdulkabeer19@gmail.com>
2023-11-02 15:30:40 +0100

    Added: vercel.json file

--- src/services/apiMovies.js
+++ src/services/apiMovies.js
@@ -1,93 +1,95 @@
-import { options } from "../utils/apiCredentials";
+import { options } from '../utils/apiCredentials';
 
 export const getPopularMovies = async (page) => {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`,
-      options
-    );
+     try {
+          const res = await fetch(
+               `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`,
+               options
+          );
 
-    const data = await res.json();
+          const data = await res.json();
 
-    return data;
-  } catch (error) {
-    throw new Error("There was an error while getting popular Movies");
-  }
+          return data;
+     } catch (error) {
+          throw new Error('There was an error while getting popular Movies');
+     }
 };
 
 export const getTopRatedMovies = async (page) => {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`,
-      options
-    );
+     try {
+          const res = await fetch(
+               `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`,
+               options
+          );
 
-    const data = await res.json();
+          const data = await res.json();
 
-    return data;
-  } catch (error) {
-    throw new Error("There was an error while getting top rated Movies");
-  }
+          return data;
+     } catch (error) {
+          throw new Error('There was an error while getting top rated Movies');
+     }
 };
 
 export const getUpcomingMovies = async (page) => {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${page}`,
-      options
-    );
+     try {
+          const res = await fetch(
+               `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${page}`,
+               options
+          );
 
-    const data = await res.json();
+          const data = await res.json();
 
-    return data;
-  } catch (error) {
-    throw new Error("There was an error while getting upcoming Movies");
-  }
+          return data;
+     } catch (error) {
+          throw new Error('There was an error while getting upcoming Movies');
+     }
 };
 
 export const getNowPlayingMovies = async (page) => {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`,
-      options
-    );
+     try {
+          const res = await fetch(
+               `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`,
+               options
+          );
 
-    const data = await res.json();
+          const data = await res.json();
 
-    return data;
-  } catch (error) {
-    throw new Error("There was an error while getting now playing Movies");
-  }
+          return data;
+     } catch (error) {
+          throw new Error(
+               'There was an error while getting now playing Movies'
+          );
+     }
 };
 
 export const getMovieById = async (id) => {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
-      options
-    );
+     try {
+          const res = await fetch(
+               `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+               options
+          );
 
-    const data = await res.json();
+          const data = await res.json();
 
-    return data;
-  } catch (error) {
-    throw new Error(
-      `There was an error while getting movie with this id: (${id})`
-    );
-  }
+          return data;
+     } catch (error) {
+          throw new Error(
+               `There was an error while getting movie with this id: (${id})`
+          );
+     }
 };
 
 export const getMovieCast = async (id) => {
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`,
-      options
-    );
+     try {
+          const res = await fetch(
+               `https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`,
+               options
+          );
 
-    const data = await res.json();
+          const data = await res.json();
 
-    return data;
-  } catch (error) {
-    throw new Error("There was an error while getting Movie Cast");
-  }
+          return data;
+     } catch (error) {
+          throw new Error('There was an error while getting Movie Cast');
+     }
 };
